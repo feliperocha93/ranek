72d60467b44cf11f038f538b8d34e9460c04d4a2
Felipe Rocha de Lucena - FLC <felipe.lucena@iteris.com.br>
2020-07-20 18:03:51 -0300

    Criação da API

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,10 +1,36 @@
 <template>
-  <p>ProdutosLista</p>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "ProdutosLista"
+  name: "ProdutosLista",
+  data: () => ({
+    produtos: null
+  }),
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto")
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r;
+        });
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
 };
 </script>
 
