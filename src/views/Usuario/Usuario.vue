bdb7687c2135522a8929da7ae7a1acc763a81baf
Felipe Rocha de Lucena - 40259869813 <felipe.lucena@anima.intranet>
2021-01-10 12:00:37 -0300

    Criação da view para usuário

--- src/views/Usuario/Usuario.vue
+++ src/views/Usuario/Usuario.vue
@@ -1,12 +1,80 @@
 <template>
-  <p>Usuario</p>
+<section class="usuario">
+  <nav class="sidenav">
+    <ul>
+      <li>
+        <router-link :to='{name: "usuario"}'>Produtos</router-link>
+      </li>
+      <li>
+        <router-link :to='{name: "compras"}'>Compras</router-link>
+      </li>
+      <li>
+        <router-link :to='{name: "vendas"}'>Vendar</router-link>
+      </li>
+      <li>
+        <router-link :to='{name: "usuario-editar"}'>Editar usuário</router-link>
+      </li>
+      <li>
+        <button @click.prevent="deslogar">Deslogar</button>
+      </li>
+    </ul>
+  </nav>
+  <transition mode="out-in">
+  <router-view></router-view>
+  </transition>
+</section>
 </template>
 
 <script>
 export default {
   name: "Usuario",
+  methods: {
+    deslogar() {
+      this.$store.dispatch("deslogarUsuario");
+      this.$router.push("/login");
+    }
+  }
 };
 </script>
 
 <style scoped>
+.usuario {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 900px;
+  margin: 40px auto;
+  grid-gap: 30px;
+  padding: 20px;
+}
+
+@media screen and (max-width: 500px) {
+  .usuario {
+  grid-template-columns: 1fr;
+  margin: 0px auto;
+}
+}
+
+.sidenav a,
+.sidenav button {
+  padding: 10px;
+  display: block;
+  background: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.sidenav a:hover,
+.sidenav button:hover,
+.sidenav a.router-link-exact-active {
+  background: #87f;
+  color: #fff;
+}
+
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  text-align: left;
+  cursor: pointer;
+}
 </style>
