9568e1e731a818ae8d84761d8cb08f4e25126b4a
Felipe Rocha de Lucena - 40259869813 <felipe.lucena@anima.intranet>
2021-01-10 12:02:31 -0300

    Implementando métodos de adição de produtos pelo usuário

--- src/store/index.js
+++ src/store/index.js
@@ -20,6 +20,7 @@ export default new Vuex.Store({
       cidade: "",
       estado: "",
     },
+    usuario_produtos: null,
   },
   mutations: {
     UPDATE_LOGIN(state, payload) {
@@ -28,8 +29,21 @@ export default new Vuex.Store({
     UPDATE_USUARIO(state, payload) {
       state.usuario = Object.assign(state.usuario, payload);
     },
+    UPDATE_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos = payload;
+    },
+    ADD_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos.unshift(payload);
+    },
   },
   actions: {
+    getUsuarioProdutos(context) {
+      api
+        .get(`/produtos?usuario_id=${context.state.usuario.id}`)
+        .then((response) => {
+          context.commit("UPDATE_USUARIO_PRODUTOS", response.data);
+        });
+    },
     getUsuario(context, payload) {
       return api.get(`/usuario/${payload}`).then(({ data }) => {
         context.commit("UPDATE_USUARIO", data);
@@ -39,6 +53,21 @@ export default new Vuex.Store({
     criarUsuario(context, payload) {
       context.commit("UPDATE_USUARIO", { id: payload.email });
       return api.post("/usuario", payload);
+    },
+    deslogarUsuario(context) {
+      context.commit("UPDATE_USUARIO", {
+        id: "",
+        nome: "",
+        email: "",
+        senha: "",
+        cep: "",
+        rua: "",
+        numero: "",
+        bairro: "",
+        cidade: "",
+        estado: "",
+      });
+      context.commit("UPDATE_LOGIN", false);
     },
   },
   modules: {},
