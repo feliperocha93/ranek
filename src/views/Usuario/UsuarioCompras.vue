8804dd6e70d4fd51fffda2fe8bd6c94f500fd49a
Felipe Rocha de Lucena - 40259869813 <felipe.lucena@anima.intranet>
2021-01-12 08:46:40 -0300

    Criando componentes de histórico de compra e venda do usuário

---
+++ src/views/Usuario/UsuarioCompras.vue
@@ -0,0 +1,61 @@
+<template>
+  <section>
+    <div v-if="compras">
+      <div class="produtos-wrapper" v-for="(compra, index) in compras" :key="index">
+        <ProdutoItem v-if="compra.produto" :produto="compra.produto">
+          <p class="vendedor"><span>Vendedor </span>{{compra.vendedor_id}}</p>
+        </ProdutoItem>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ProdutoItem from '@/components/ProdutoItem.vue';
+import { api } from '@/service.js';
+import { mapState } from 'vuex';
+
+export default {
+  name: "UsuarioCompras",
+  components: {
+    ProdutoItem
+  },
+  data: () => ({
+    compras: null
+  }),
+  computed: {
+    ...mapState(["usuario", "login"])
+  },
+  methods: {
+    getCompras() {
+      api.get(`transacao?comprador_id=${this.usuario.id}`).then((response) => {
+        this.compras = response.data;
+      })
+    }
+  },
+  watch: {
+    login() {
+      this.getCompras();
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getCompras();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/views/Usuario/UsuarioVendas.vue
@@ -0,0 +1,84 @@
+<template>
+  <section>
+    <div v-if="vendas">
+      <div class="produtos-wrapper" v-for="(venda, index) in vendas" :key="index">
+        <ProdutoItem v-if="venda.produto" :produto="venda.produto">
+          <p class="vendedor">
+            <span>Comprador: </span>
+            {{venda.comprador_id}}
+          </p>
+        </ProdutoItem>
+        <div class="entrega">
+          <h3>Entrega:</h3>
+          <ul v-if="venda.endereco">
+            <li v-for="(value, key) in venda.endereco" :key="key">
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ProdutoItem from '@/components/ProdutoItem.vue';
+import { api } from '@/service.js';
+import { mapState } from 'vuex';
+
+export default {
+  name: "UsuarioVendas",
+  components: {
+    ProdutoItem
+  },
+  data: () => ({
+    vendas: null
+  }),
+  computed: {
+    ...mapState(["usuario", "login"])
+  },
+  methods: {
+    getVendas() {
+      api.get(`transacao?vendedor_id=${this.usuario.id}`).then((response) => {
+        this.vendas = response.data;
+      })
+    }
+  },
+  watch: {
+    login() {
+      this.getVendas();
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getVendas();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80;
+}
+
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  grid-gap: 20px;
+  margin-bottom: 60px;
+} 
+
+h2 {
+  margin-bottom: 20px;
+}
+
+h3 {
+  justify-self: end;
+  margin: 0;
+}
+</style>
