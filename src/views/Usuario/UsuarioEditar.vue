f52b96f45edb528eb8cb69bd3b66409296ac2aa5
Felipe Rocha de Lucena - 40259869813 <felipe.lucena@anima.intranet>
2021-01-24 18:45:42 -0300

    Ajustado método para atualizar usuário

--- src/views/Usuario/UsuarioEditar.vue
+++ src/views/Usuario/UsuarioEditar.vue
@@ -1,33 +1,36 @@
 <template>
   <section>
     <UsuarioForm>
-      <button class="btn" @click.prevent="atualizarUsuario">Atualizar Usuário</button>
+      <button class="btn" @click.prevent="atualizarUsuario">
+        Atualizar Usuário
+      </button>
     </UsuarioForm>
   </section>
 </template>
 
 <script>
-import UsuarioForm from '@/components/UsuarioForm.vue';
+import UsuarioForm from "@/components/UsuarioForm.vue";
 import { api } from "@/service.js";
 
 export default {
   name: "UsuarioEditar",
   components: {
-    UsuarioForm
+    UsuarioForm,
   },
   methods: {
     atualizarUsuario() {
-      api.put(`/usuario/${this.$store.state.usuario.id}`, this.$store.state.usuario).then(() => {
-        this.$store.dispatch("getUsuario");
-        this.$router.push({name: "usuario"});
-      }).catch(error => {
-        console.log(error.response);
-      });
-    }
-  }
+      api
+        .put(`/usuario/`, this.$store.state.usuario)
+        .then(() => {
+          this.$store.dispatch("getUsuario");
+          this.$router.push({ name: "usuario" });
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
